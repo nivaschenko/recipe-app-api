cb2a0592c7d70e790879ab22ba6d8a467db53b0f
nivashchenko <ivaschenko.n@gmail.com>
2024-01-11 04:22:44 +0200

    finish with user API

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,44 @@
-from django.contrib import admin  # noqa
+"""
+Django admin customisation.
+"""
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.utils.translation import gettext_lazy as _
 
-# Register your models here.
+from core.models import User
+
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    """Define the admin pages for the users."""
+    ordering = ['id']
+    list_display = ['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            )
+        }),
+    )
